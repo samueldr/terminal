68c35076f8bb3ae024bf2c4318080eae7cfc4b53
Samuel Dionne-Riel <samuel@dionne-riel.com>
2026-05-10 18:01:10 -0400

    bin/terminal: Handle `-x` like xterm does...
    ... and not much else.

--- src/bin/terminal.rs
+++ src/bin/terminal.rs
@@ -1,8 +1,9 @@
-use glib::debug;
+use glib::{debug, error};
 use gtk::{Application, gio, glib};
+use std::ffi::OsString;
 use vte4::prelude::*;
 
-use samueldr_terminal_ii::misc::G_LOG_DOMAIN;
+use samueldr_terminal_ii::misc::{G_LOG_DOMAIN, get_shell};
 use samueldr_terminal_ii::terminal;
 
 const APP_ID: &str = "com.samueldr.terminal";
@@ -38,6 +39,69 @@ fn main() -> glib::ExitCode {
             std::env::vars().for_each(|(name, _)| std::env::remove_var(name));
         }
 
+        // Sadly, we can't let GLib/gio handle the parameters for xterm compatibility.
+        // There is no way to mark `-e` or `-x` as an argument that stops further processing.
+        // The `--` delimiter can be used for that, but will be added to `cmdline.arguments()`.
+        // So instead, we can "just" process the args ourselves.
+
+        // Create a binding for args without argv[0].
+        let args: Vec<OsString> = cmdline.arguments().into_iter().skip(1).collect();
+
+        // Find out where we split the `-x` argv.
+        // Fallback to "split" at the end, leaving an empty list.
+        // We're keeping it as an `Option` so we can detect if the flag was given.
+        let exec_index = args.iter().position(|arg| {
+            arg == "-e" || // xterm
+                    arg == "-x" // lilyterm and others
+        });
+
+        // Do the split.
+        let (args, command_args) = args.split_at(exec_index.unwrap_or(args.len()));
+
+        // Drop the `-e/-x` arg from the command arguments.
+        let command_args: Vec<String> = command_args
+            .iter()
+            .skip(1)
+            .cloned()
+            .map(|s| {
+                s.into_string()
+                    .expect("unexpectedly coudln't handle argument.")
+            })
+            .collect();
+
+        // Early check for a valid exec command line.
+        if exec_index.is_some() && command_args.is_empty() {
+            error!("No command given while command flag (-e/-x) was given.");
+            return glib::ExitCode::FAILURE;
+        }
+
+        // Then handle params...
+        for arg in args.iter() {
+            // Though other than `-x` we don't handle any.
+            let result = match arg.to_str().unwrap_or("") {
+                "--help" | "-h" => {
+                    println!("Sorry. No help for you.");
+                    println!("Though `-x` works like with xterm.");
+                    println!("That's it.");
+                    Some(glib::ExitCode::SUCCESS)
+                }
+                _ => {
+                    error!("Unexpected argument: {:?}", arg);
+                    Some(glib::ExitCode::FAILURE)
+                }
+            };
+            if let Some(exit) = result {
+                return exit;
+            }
+        }
+
+        // "Fall" back onto the user's login shell.
+        let cmd: Vec<String> = if command_args.is_empty() {
+            vec![get_shell(), "-".into()]
+        } else {
+            command_args
+        };
+
         // Get the environment in a more idiomatic shape to shuttle around.
         let mut environ: Vec<String> = cmdline
             .environ()
@@ -45,14 +109,18 @@ fn main() -> glib::ExitCode {
             .map(|s| s.into_string().expect("unexpectedly couldn't handle env."))
             .collect();
 
+        // The CWD comes from the remote instance.
+        let cwd = cmdline.cwd().expect("unexpectedly got no cwd.");
+
         // Make sure `TERM` is valid for VTE.
         environ.push("TERM=screen-256color".into());
 
-        terminal::create(
-            app,
-            cmdline.cwd().expect("unexpectedly got no cwd."),
-            environ,
-        );
+        debug!("Terminal command-line processing results:");
+        debug!("  exec?: {:?}", exec_index.is_some());
+        debug!("  args: {:?}", args);
+        debug!("  command: {:?}", cmd);
+
+        terminal::create(app, cwd, environ, cmd);
 
         glib::ExitCode::SUCCESS
     });
